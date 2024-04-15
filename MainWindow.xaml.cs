a836bc6e6107762c8d74fb9bfa73103276897f05
varashus <115972590+varashus@users.noreply.github.com>
2024-04-15 11:17:33 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            ElvListBox.Items.Remove(ElvListBox.SelectedItem);
+           
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            ElvListBox.Items.Add(New.Text);
+        }
+
+        private void Change_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (ElvListBox.SelectedItem == null)
+            {
+                MessageBox.Show("Nincs kiválasztva elem");
+            }
+            else
+            {
+                int Index = ElvListBox.SelectedIndex;
+                ElvListBox.Items.RemoveAt(Index);
+                ElvListBox.Items.Add((New.Text).ToString());
+            }
+
+        }
+    }
+}
